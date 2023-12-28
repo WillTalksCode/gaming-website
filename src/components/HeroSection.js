c6f1ca2ff7e5c05886eb909d0feb9a25a7ee4d81
William Talley <williamtalley51@gmail.com>
2023-12-28 00:38:56 -0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src="./videos/reactsitetrailer.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
+      <h1>Enter A World of Gaming!</h1>
       <p>What are you waiting for?</p>
       <div className="hero-btns">
         <Button
@@ -15,7 +15,7 @@ function HeroSection() {
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          GET STARTED
+          Check it out!
         </Button>
         <Button
           className="btns"
